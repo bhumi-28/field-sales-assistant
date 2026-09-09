d1f9a852ea38ae69dee5820f039fa631ec6ca4ab
bhumi-28 <bhumishetty275@gmail.com>
2026-09-09 12:18:49 +0530

    add spring boot skeleton

---
+++ backend/src/main/java/com/intellect/field_sales_backend/FieldSalesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.intellect.field_sales_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldSalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldSalesBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/intellect/field_sales_backend/FieldSalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellect.field_sales_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldSalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
